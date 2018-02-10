d11eecc35ec64475ca82c4582b1252caf6c4b1d4
Jesse Eldell <31395028+EJ24356@users.noreply.github.com>
2018-02-10 14:11:03 -0500

    Add files via upload

--- Exercise.java
+++ Exercise.java
@@ -1,10 +1,16 @@
+import java.util.Random;
+
 
 public class Exercise {
 
-	String name;
+	public String name;
 	int sets;
 	int reps;
+	private static int arrayIndex = 0;
 	
+	enum EXERCISES { pushups, situps, squats, lunges , crunches, leg_raises, dips , 
+		pullups , reverse_crunches, scissors, flutter_kicks , butterflys }
+	static String[] EX_ARRAY = new String[EXERCISES.values().length];
 	public Exercise() {
 		name = "";
 		sets = 0;
@@ -23,7 +29,65 @@ public class Exercise {
 		reps = r;
 	}
 	
+	public static void shuffle()
+	{
+		Random ran = new Random();
+          int index1, index2;
+		
+		for(int i=0; i<17; i++) //Performs 80 random "swaps" of indices.
+		{
+			
+			index1 = ran.nextInt(11); //random index between 0-19
+			index2 = ran.nextInt(11); //random index between 0-19
+			swap(index1, index2);
+		}
+		
+	}//shuffle
+	
+	public static void swap(int a, int b) //swaps items @ indices a & b in WORD_ARRAY
+	{
+		String temp = EX_ARRAY[a];
+		EX_ARRAY[a] = EX_ARRAY[b];
+		EX_ARRAY[b] = temp;
+	}//swap
 	
 	
+	public static void ExerciseARRAY()
+	{
+		int i = 0;
+		for (EXERCISES e: EXERCISES.values())
+		{
+			EX_ARRAY[i] = e.toString();
+			i++;
+		}
+		shuffle();
+	}//EXERCISE_ARRAY
 	
-}
+	public static String new_Exercise()
+	{
+		if(arrayIndex == 0)
+			ExerciseARRAY();
+		
+		String myExercise = EX_ARRAY[arrayIndex];
+		arrayIndex++;
+		return myExercise;
+		
+	}
+	
+	public int new_set()
+	{
+		Random s =new Random();
+		while(sets<=1)
+		sets = s.nextInt(5);
+		return sets;
+	}
+	
+	public int new_rep()
+	{
+		Random r =new Random();
+		while(reps<=1)
+		reps = r.nextInt(10);
+		return reps;
+	}
+	
+}//EXERCISE
--- WorkOut.java
+++ WorkOut.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class WorkOut {
 
+	static boolean set = false;
+	static boolean rep = false;
+	
 	public static void main(String[] args) {
 
+		
+System.out.println(	"	  _   _   _   _   _   _   _  ");
+System.out.println(	"	 / \\ / \\ / \\ / \\ / \\ / \\ / \\ ");
+System.out.println(	"	( W | o | r | k | o | u | t )");
+System.out.println(	"         \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/");
+		
 		Scanner scan = new Scanner(System.in);
 
 		String name;
@@ -12,6 +21,7 @@ public class WorkOut {
 
 		int numEx = 0;
 		boolean b = true;
+
 		while (b) {
 			System.out.print("How many exercises? ");
 			if (scan.hasNextInt())
@@ -19,38 +29,80 @@ public class WorkOut {
 			if (numEx > 0) {
 				b = false;
 			} else {
-				System.out.print("Please enter a number");
+				System.out.println("Please enter a number");
 				scan = new Scanner(System.in);
 			}
 
 		}
 
 		Exercise[] arr = new Exercise[numEx];
-
+    System.out.println("Randomize or Customize?  R/C");
+      Scanner input = new Scanner (System.in);
+          String ans= input.next();
+          if (!(ans.equalsIgnoreCase("Customize")||ans.equalsIgnoreCase("C")||
+        		  ans.equalsIgnoreCase("Randomize")||ans.equalsIgnoreCase("R")))
+        	  System.exit(0);
+          if (ans.equalsIgnoreCase("Customize")||ans.equalsIgnoreCase("C"))
+          {
+        	do
+        	{
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = new Exercise();
 			System.out.println("Exercise: " + (i + 1));
+			
 			System.out.print("Name of exercise: ");
-			Scanner n = new Scanner(System.in);
-			name = n.next();
-			arr[i].setName(name);
+			name = scan.next();
+			arr[i].setName(name);//name
+			
+			while(!set){
 			System.out.print("Number of sets: ");
+			if(scan.hasNextInt()) {
 			sets = scan.nextInt();
 			arr[i].setSets(sets);
+			set=true;
+			}
+			else {
+				System.out.println("Please enter a number");
+				scan = new Scanner(System.in);
+			}
+			 }//set
+			
+			while(!rep){
 			System.out.print("Number of reps: ");
+			if(scan.hasNextInt()) {
 			reps = scan.nextInt();
 			arr[i].setReps(reps);
-		}
-
-		String response;
-		System.out.print("Are you ready to begin program?");
+			rep=true;
+			b=true;
+			}
+			else{
+				System.out.println("Please enter a number");
+				scan = new Scanner(System.in);
+			}
+		 }	//rep while	
+		}//for		
+		   }while(!b);//while
+          }//customize
+          
+          else if (ans.equalsIgnoreCase("Randomize")||ans.equalsIgnoreCase("R"))
+          {
+        	  for (int i=0;i<arr.length;i++)
+        	  {
+        		  arr[i]=new Exercise();
+        		  String exercise = Exercise.new_Exercise();
+        	  arr[i].setName(exercise);
+        	  arr[i].setSets(arr[i].new_set());
+        	  arr[i].setReps(arr[i].new_rep());
+        	  }
+          }//randomize
+          
+      	String response;
+		System.out.print("Are you ready to begin program? Y/N"+"\n");
 		response = scan.next();
-		if (response.equals("yes"))
+		if (response.equalsIgnoreCase("yes")||response.equalsIgnoreCase("y"))
 			for (int i = 0; i < arr.length; i++) {
 				System.out.print(arr[i].name + ": " + arr[i].sets + " sets, " + arr[i].reps + " reps");
 				System.out.println();
 			}
-
-	}
-
-}
+         }//main
+}//WorkOut
